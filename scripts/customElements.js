b7a8868793a6b1375c40a5a43952548aff1df533
Nathan Wong <56657800+NathanWong1106@users.noreply.github.com>
2021-06-17 14:33:33 -0400

    scroll spy and more

--- scripts/customElements.js
+++ scripts/customElements.js
@@ -1,7 +1,38 @@
-const runFuncForAllClasses = (func, className) => {
-    let elems = document.getElementsByClassName(className);
+class Sidebar {
+
+    constructor(id, elem, isOpen=false) {
+        this.id = id;
+        this.elem = elem;
+        this.isOpen = isOpen;
+    }
+
+    toggle = () => {
+        let smallScreen = window.matchMedia("(max-width: 600px)");
+
+        if(this.isOpen){
+            this.elem.style.width = "0";
+            this.isOpen = false;
+            this.elem.style.backgroundColor = "rgba(0,0,0,0)"
+        }
+        else if(smallScreen.matches){
+            this.elem.style.width = "80vw";
+            this.elem.style.backgroundColor = "white"
+            this.isOpen = true;
+        } else {
+            this.elem.style.width = "10vw";
+            this.elem.style.backgroundColor = "white"
+            this.isOpen = true;
+        }
+    }
+}
+
+let sidebars = {};
+
+const runFuncForAll = (func, query) => {
+    let elems = document.querySelectorAll(query);
     Array.prototype.forEach.call(elems, func);
 }
+
 
 const playAnimOnVisible = (elem, animClassName) => {
     if(elem.classList.contains("is-inview")){
@@ -23,27 +54,76 @@ const playAnimOnVisible = (elem, animClassName) => {
     observer.observe(elem, {attributes: true});
 }
 
+const addScrollSpy = (elem) => {
+    const observer = new MutationObserver(mutations => {
+        mutations.forEach(mutation => {
+            if(mutation.type !== "attributes" && mutation.attributeName !== "class"){
+                return;
+            } else {
+                if(mutation.target.classList.contains("is-inview")){
+                    clearActiveNavs();
+                    runFuncForAll(nav => {
+                        if(nav.dataset.spytarget.slice(1) === elem.id){
+                            nav.classList.add("active");
+                        }
+                    }, ".scrollspy-nav")
+                }
+            }
+        })
+    })
+    observer.observe(elem, {attributes: true});
+}
+
 const scrollButtonFunc = button => {
     button.onclick = () => {
         scroll.scrollTo(document.getElementById(button.dataset.scrollto));
     }
 }
 
+const clearActiveNavs = () => {
+    runFuncForAll((nav) => {
+        nav.classList.remove("active");
+    }, ".scrollspy-nav")
+}
+
 const titlesFunc = title => {
     playAnimOnVisible(title, "title-init");
+    addScrollSpy(title);
 }
 
 const articlesFunc = article => {
     playAnimOnVisible(article, "article-init");
 }
 
+const sidebarToggleFunc = toggle => {
+    toggle.onclick = () => {
+        let bar = sidebars[toggle.dataset.sidebar];
+        bar.toggle();
+    }
+}
+
+const scrollspyNavFunc = nav => {
+    nav.onclick = () => {
+        scroll.scrollTo(document.querySelector(nav.dataset.scrollto));
+    }
+}
+
+// Gather all sidebar togglers
+runFuncForAll((sidebar) => {
+    sidebars[sidebar.id] = new Sidebar(sidebar.id, sidebar);
+}, ".sidebar")
 
 // Scroll buttons
-runFuncForAllClasses(scrollButtonFunc, "scroll-button");
+runFuncForAll(scrollButtonFunc, ".scroll-button");
+
+// Scrollspy navs
+runFuncForAll(scrollspyNavFunc, ".scrollspy-nav");
 
 // Titles
-runFuncForAllClasses(titlesFunc, "title");
+runFuncForAll(titlesFunc, ".title");
 
 // Articles
-runFuncForAllClasses(articlesFunc, "article");
+runFuncForAll(articlesFunc, ".article");
 
+// Sidebar toggles
+runFuncForAll(sidebarToggleFunc, ".sidebar-toggle");
